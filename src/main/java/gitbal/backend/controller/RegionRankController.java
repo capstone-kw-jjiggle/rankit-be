af880f78c944ab2b61bb32cea88eec972781c5f0
Khyojun <khyojun1104@gmail.com>
2024-03-12 10:47:15 +0900

    [feat] - #10 컨트롤러 초기 세팅

---
+++ src/main/java/gitbal/backend/controller/RegionRankController.java
@@ -0,0 +1,11 @@
+package gitbal.backend.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/regionRank")
+public class RegionRankController {
+
+}
