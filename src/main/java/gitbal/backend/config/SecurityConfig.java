73dc098e77a1027cb2b7307874f4beec99fb1d3f
Khyojun <khyojun1104@gmail.com>
2024-03-22 17:49:13 +0900

    [feat] #20 - jwt 설정을 위한 security, controller test url 선정
    테스트를 하기 위한 /api/v1/loginCheck 로 접속하면 인증 과정 진행

--- src/main/java/gitbal/backend/config/SecurityConfig.java
+++ src/main/java/gitbal/backend/config/SecurityConfig.java
@@ -1,19 +1,24 @@
 package gitbal.backend.config;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
+import gitbal.backend.security.CustomOAuth2UserService;
+import gitbal.backend.security.OAuth2AuthenticationSuccessHandler;
+import gitbal.backend.security.jwt.JwtAuthenticationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2AuthenticationSuccessHandler oAuth2AuthenticationSuccessHandler;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -22,11 +27,15 @@ public class SecurityConfig {
             .authorizeHttpRequests(
                 auth -> {
                     auth.requestMatchers("/swagger-ui/**", "/").permitAll();
-                    auth.requestMatchers(PathRequest.toStaticResources().atCommonLocations());
-                    auth.anyRequest().authenticated();
+                    auth.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll();
+                    auth.requestMatchers("/api/v1/loginCheck").authenticated(); // TODO : 임의의 로직으로 loginChecking 진행중임.
                 }
             )
-            .oauth2Login(withDefaults())
+            .oauth2Login(oauth2 -> {
+                oauth2.userInfoEndpoint(user -> user.userService(customOAuth2UserService));
+                oauth2.successHandler(oAuth2AuthenticationSuccessHandler);
+            })
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
 
