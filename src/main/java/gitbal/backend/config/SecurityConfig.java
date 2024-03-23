41e35dd3e86720d7054149833598d79985fac2c8
Khyojun <khyojun1104@gmail.com>
2024-03-23 10:24:04 +0900

    [feat] #20 - jwt 인증시 세션 관련해서 인증하지 않도록 하는 정책 추가

--- src/main/java/gitbal/backend/config/SecurityConfig.java
+++ src/main/java/gitbal/backend/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -27,8 +28,10 @@ public class SecurityConfig {
             .authorizeHttpRequests(
                 auth -> {
                     auth.requestMatchers("/swagger-ui/**", "/").permitAll();
-                    auth.requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll();
-                    auth.requestMatchers("/api/v1/loginCheck").authenticated(); // TODO : 임의의 로직으로 loginChecking 진행중임.
+                    auth.requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                        .permitAll();
+                    auth.requestMatchers("/api/v1/loginCheck")
+                        .authenticated(); // TODO : 임의의 로직으로 loginChecking 진행중임.
                     auth.anyRequest().permitAll();
                 }
             )
@@ -36,6 +39,8 @@ public class SecurityConfig {
                 oauth2.userInfoEndpoint(user -> user.userService(customOAuth2UserService));
                 oauth2.successHandler(oAuth2AuthenticationSuccessHandler);
             })
+            .sessionManagement(
+                session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
             .build();
     }
