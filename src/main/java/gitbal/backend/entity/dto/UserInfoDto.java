78b324b184cdf9186091703b0848af3aad55ae77
Khyojun <38220795+khyojun@users.noreply.github.com>
2024-04-03 01:22:47 +0900

    [feat] #27 -  점수 계산에 필요한 issue, follower, repoCount 추가

--- src/main/java/gitbal/backend/entity/dto/UserInfoDto.java
+++ src/main/java/gitbal/backend/entity/dto/UserInfoDto.java
@@ -1,12 +1,12 @@
 package gitbal.backend.entity.dto;
 
 
-
-public record UserInfoDto(Long prCount, Long commitCount){
-    public static UserInfoDto of(Long prCount, Long commitCount) {
-        return new UserInfoDto(prCount, commitCount);
+public record UserInfoDto(Long prCount, Long commitCount, Long issues,
+                          Long followerCount, Long repoCount) {
+    public static UserInfoDto of(Long prCount, Long commitCount, Long issues,
+        Long followerCount, Long repoCount) {
+        return new UserInfoDto(prCount, commitCount, issues, followerCount, repoCount);
     }
-
 
 
 }
