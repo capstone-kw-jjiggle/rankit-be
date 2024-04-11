7168dae7b3fb74dcacf1f2217a0ddc7151b0ae46
Khyojun <khyojun1104@gmail.com>
2024-04-11 17:14:32 +0900

    [fix] #43 - 잘못된 변수 이름 수정
    schoolScore -> regionScore

--- src/main/java/gitbal/backend/entity/dto/RegionRankDto.java
+++ src/main/java/gitbal/backend/entity/dto/RegionRankDto.java
@@ -1,6 +1,6 @@
 package gitbal.backend.entity.dto;
 
-public record RegionRankDto(String regionName, Long schoolScore) {
+public record RegionRankDto(String regionName, Long regionScore) {
 
     public static RegionRankDto of(String regionName, Long regionScore) {
         return new RegionRankDto(regionName, regionScore);
