17cf1edba71fd83ab3d16d3eb1098692ea7e0bb5
leesj000603 <leesj000603@daum.net>
2024-04-30 15:26:17 +0900

    [feat] #51 - regionRank/firstRegion

--- src/main/java/gitbal/backend/controller/RegionRankController.java
+++ src/main/java/gitbal/backend/controller/RegionRankController.java
@@ -1,6 +1,7 @@
 package gitbal.backend.controller;
 
 
+import gitbal.backend.entity.dto.FirstRankRegionDto;
 import gitbal.backend.entity.dto.RegionListDto;
 import gitbal.backend.entity.dto.RegionListPageResponseDto;
 import gitbal.backend.service.RegionRankService;
@@ -16,32 +17,41 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/regionRank")
 @RequiredArgsConstructor
-@Tag(name= "지역 랭킹 API(구현중)", description = "지역 랭킹 페이지를 위한 api입니다.")
+@Tag(name = "지역 랭킹 API(구현중)", description = "지역 랭킹 페이지를 위한 api입니다.")
 public class RegionRankController {
-    private final RegionRankService regionRankService;
 
-    @GetMapping("/myRegion")
-    @Operation(summary = "내 지역 정보", description = "내 지역 정보를 나타내는 api입니다.")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "내 지역 정보 요청을 성공했습니다."),
-        @ApiResponse(responseCode = "5xx", description = "내 지역 정보 요청을 실패했습니다.")
-    })
-    public void myRegion(){ // TODO : 로그인한 유저로 봐야함.
+  private final RegionRankService regionRankService;
 
-    }
+  @GetMapping("/myRegion")
+  @Operation(summary = "내 지역 정보 (미구현)", description = "내 지역 정보를 나타내는 api입니다.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "내 지역 정보 요청을 성공했습니다."),
+      @ApiResponse(responseCode = "5xx", description = "내 지역 정보 요청을 실패했습니다.")
+  })
+  public void myRegion() { // TODO : 로그인한 유저로 봐야함.
+
+  }
+
+  @GetMapping("/firstregion")
+  @Operation(summary = "1등 지역 정보 (구현 완료)", description = "1등 지역 정보 탭 관련 api 요청입니다.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "1등 지역 정보 요청을 성공했습니다."),
+      @ApiResponse(responseCode = "5xx", description = "1등 지역 정보 요청을 실패했습니다.")
+  })
+  public FirstRankRegionDto firstRankRegionInfo() {
+    return regionRankService.getFirstRankRegionnfo();
+  }
 
 
-    @GetMapping("/regionList")
-    @Operation(summary = "지역 리스트(구현 완료)", description = "지역 리스트를 나타내는 api입니다.")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "지역 리스트 요청을 성공했습니다."),
-        @ApiResponse(responseCode = "5xx", description = "지역 리스트 요청을 실패했습니다.")
-    })
-    public RegionListPageResponseDto<RegionListDto> regionList(){
-        return regionRankService.getRegionList();
-    }
-
-
+  @GetMapping("/regionList")
+  @Operation(summary = "지역 리스트 (구현 완료)", description = "지역 리스트를 나타내는 api입니다.")
+  @ApiResponses(value = {
+      @ApiResponse(responseCode = "200", description = "지역 리스트 요청을 성공했습니다."),
+      @ApiResponse(responseCode = "5xx", description = "지역 리스트 요청을 실패했습니다.")
+  })
+  public RegionListPageResponseDto<RegionListDto> regionList() {
+    return regionRankService.getRegionList();
+  }
 
 //    /api/v1/regionRank/myRegion	get
 ///api/v1/regionRank/regionList	get
--- src/main/java/gitbal/backend/service/RegionRankService.java
+++ src/main/java/gitbal/backend/service/RegionRankService.java
@@ -1,6 +1,9 @@
 package gitbal.backend.service;
 
 import gitbal.backend.entity.Region;
+import gitbal.backend.entity.School;
+import gitbal.backend.entity.dto.FirstRankRegionDto;
+import gitbal.backend.entity.dto.FirstRankSchoolDto;
 import gitbal.backend.entity.dto.RegionListDto;
 import gitbal.backend.entity.dto.RegionListPageResponseDto;
 import gitbal.backend.repository.RegionRepository;
@@ -24,7 +27,7 @@ public class RegionRankService {
   public RegionListPageResponseDto<RegionListDto> getRegionList() {
     Sort sort = Sort.by("score").descending();
     int page = 1;
-    Pageable pageable = PageRequest.of(page -1, 10, sort);
+    Pageable pageable = PageRequest.of(page - 1, 10, sort);
     Page<Region> regionPage = regionRepository.findAll(pageable);
 
     List<RegionListDto> regionDtoList = regionPage.stream()
@@ -34,6 +37,16 @@ public class RegionRankService {
     // 3. PageResponseDto 생성
     return RegionListPageResponseDto.<RegionListDto>withALl()
         .regionList(regionDtoList)
+        .build();
+  }
+
+  public FirstRankRegionDto getFirstRankRegionnfo() {
+    Region firstRegion = regionRepository.firstRankedRegion(); // TODO: 우선 가장 높은 점수의 학교를 가져오는 쿼리로 가져옴. (나중엔 미리 점수별로 정렬해둘 것이므로 수정)
+    return FirstRankRegionDto.builder()
+        .regionName(firstRegion.getRegionName())
+        .regionScore(firstRegion.getScore())
+        .regionChangeScore(null)
+        .mvpName(firstRegion.getTopContributor())
         .build();
   }
 
