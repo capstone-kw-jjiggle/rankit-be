55d6420e74bf3ca649f138e173c9b1138d9b79b4
leesj000603 <leesj000603@daum.net>
2024-04-30 15:26:17 +0900

    [feat] #51 - regionRank/firstRegion

--- src/main/java/gitbal/backend/repository/RegionRepository.java
+++ src/main/java/gitbal/backend/repository/RegionRepository.java
@@ -9,18 +9,24 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface RegionRepository extends JpaRepository<Region, Long> {
-    Optional<Region> findByRegionName(String regionName);
+
+  Optional<Region> findByRegionName(String regionName);
 
 
-    @Query("SELECT count(r) FROM Region r WHERE r.score > :regionScore")
-    int regionScoreRacedForward(@Param("regionScore") Long regionScore);
+  @Query("SELECT count(r) FROM Region r WHERE r.score > :regionScore")
+  int regionScoreRacedForward(@Param("regionScore") Long regionScore);
 
-    @Query("SELECT count(r) FROM Region r WHERE r.score < :regionScore")
-    int regionScoreRacedBackward(@Param("regionScore") Long regionScore);
+  @Query("SELECT count(r) FROM Region r WHERE r.score < :regionScore")
+  int regionScoreRacedBackward(@Param("regionScore") Long regionScore);
 
-    @Query(value = "(SELECT * FROM region WHERE score < :regionScore ORDER BY score DESC LIMIT :behind)" +
-        " UNION ALL " +
-        "(SELECT * FROM region WHERE score > :regionScore ORDER BY score ASC LIMIT :front)",
-        nativeQuery = true)
-    List<Region> regionScoreRaced(@Param("regionScore") Long regionScore, @Param("front") int fowrardCount, @Param("behind") int backwardCount);
+  @Query(value =
+      "(SELECT * FROM region WHERE score < :regionScore ORDER BY score DESC LIMIT :behind)" +
+          " UNION ALL " +
+          "(SELECT * FROM region WHERE score > :regionScore ORDER BY score ASC LIMIT :front)",
+      nativeQuery = true)
+  List<Region> regionScoreRaced(@Param("regionScore") Long regionScore,
+      @Param("front") int fowrardCount, @Param("behind") int backwardCount);
+
+  @Query("SELECT r FROM Region r ORDER BY r.score DESC LIMIT 1")
+  Region firstRankedRegion();
 }
