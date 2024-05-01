9132088220d650ad73ba6ac7a62725735a80d230
Khyojun <khyojun1104@gmail.com>
2024-05-02 01:30:15 +0900

    [feat] #54 -  PageCalculator 계산 공식 수정
    예외 케이스 추가 발견 -> startPage : 1, endPage : 1 과 같은 경우에 대하여 오류가 발생하여 오류 처리

--- src/main/java/gitbal/backend/domain/PageCalculator.java
+++ src/main/java/gitbal/backend/domain/PageCalculator.java
@@ -6,21 +6,29 @@ import org.springframework.data.domain.Page;
 public class PageCalculator {
 
     private static final int PAGE_RANGE = 5;
+    private static final int MINIMUM_MEDIAN_PAGE=3;
 
     public static PageInfoDto calculatePageInfo(Page page) {
         int currentPage = page.getNumber() + 1;
         int totalPage = page.getTotalPages();
-        int startPage = Math.max(1, currentPage - 2);
-        int endPage = Math.min(currentPage + 2, totalPage);
+        int startPage = Math.max(1, currentPage - PAGE_RANGE/2);
+        int endPage = Math.min(currentPage + PAGE_RANGE/2, totalPage);
 
-        if (currentPage - startPage < PAGE_RANGE / 2) {
-            endPage = startPage + (PAGE_RANGE - 1);
-        }
-        if (endPage - currentPage < PAGE_RANGE / 2) {
-            startPage = endPage - (PAGE_RANGE - 1);
-        }
+        if(isCurrentPageLessthanMinimumMedianPage(currentPage, totalPage))
+            endPage=PAGE_RANGE;
+        if(isCurrentPageLargerthanMaximumMedianPage(currentPage, totalPage))
+            startPage=totalPage-PAGE_RANGE+1;
+
 
         return new PageInfoDto(currentPage, startPage, endPage, totalPage);
+    }
+
+    private static boolean isCurrentPageLargerthanMaximumMedianPage(int currentPage, int totalPage) {
+        return currentPage > totalPage - PAGE_RANGE / 2 && totalPage >= PAGE_RANGE;
+    }
+
+    private static boolean isCurrentPageLessthanMinimumMedianPage(int currentPage, int totalPage) {
+        return currentPage < MINIMUM_MEDIAN_PAGE && totalPage >= PAGE_RANGE;
     }
 
 }
