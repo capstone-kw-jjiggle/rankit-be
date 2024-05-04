53aba32af0e1bfa3db53e89a45e1605b3fa5a041
Khyojun <khyojun1104@gmail.com>
2024-05-04 14:56:51 +0900

    [chore] #53 - swagger 관련 문서 mainPageController에 작성

--- src/main/java/gitbal/backend/controller/MainPageController.java
+++ src/main/java/gitbal/backend/controller/MainPageController.java
@@ -17,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/mainPage")
 @RequiredArgsConstructor
-@Tag(name = "메인페이지 API(구현중)", description = "메인페이지에 필요한 정보를 위한 api입니다.")
+@Tag(name = "메인페이지 API(구현 일부 완료 -> 에러 핸들링, 기획 완료에 따라 진행 예정)", description = "메인페이지에 필요한 정보를 위한 api입니다.")
 public class MainPageController {
 
     private final MainPageService mainPageService;
@@ -34,7 +34,7 @@ public class MainPageController {
 
 
     @GetMapping("/firstRank")
-    @Operation(summary = "메인 페이지에 나타나는 1등 학교, 지역을 나타내는 API입니다.", description = "메인에 있는 1등을 나타내기 위한 API")
+    @Operation(summary = "메인 페이지에 나타나는 1등 학교, 지역을 나타내는 API입니다.(구현 완료)", description = "메인에 있는 1등을 나타내기 위한 API")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "메인 페이지 1등 표시 지역, 학교 표시에 성공했습니다."),
         @ApiResponse(responseCode = "5xx", description = "메인 페이지 1등 표시 지역, 학교 표시에 실패했습니다.")
