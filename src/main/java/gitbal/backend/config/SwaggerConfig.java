602cedfaddb69f2f1e1be33949ea98dc8c268fb0
Khyojun <khyojun1104@gmail.com>
2024-05-06 16:33:52 +0900

    [chore] #70 - swagger에 jwt 토큰 넣어서 인증한 상태로 진행할 수 있게 추가

--- src/main/java/gitbal/backend/config/SwaggerConfig.java
+++ src/main/java/gitbal/backend/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package gitbal.backend.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,10 +13,15 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openApi(){
-        return new OpenAPI()
-            .components(new Components())
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+            .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
             .info(apiInfo());
+    }
 
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+            .bearerFormat("JWT")
+            .scheme("bearer");
     }
 
     private Info apiInfo() {
