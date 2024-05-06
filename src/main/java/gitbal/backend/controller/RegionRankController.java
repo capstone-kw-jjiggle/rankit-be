e4607a27f9dee63c910c984e75bf85401e32975a
Khyojun <khyojun1104@gmail.com>
2024-05-06 21:43:48 +0900

    [feat] #64 - /api/v1/regionRank/myRegion
    현재 로컬에서는 작동되는 것 확인됨. 이후 fe api 테스트 진행시 오류가 발생할 경우 수정할 예정

--- src/main/java/gitbal/backend/controller/RegionRankController.java
+++ src/main/java/gitbal/backend/controller/RegionRankController.java
@@ -2,6 +2,7 @@ package gitbal.backend.controller;
 
 
 import gitbal.backend.entity.dto.FirstRankRegionDto;
+import gitbal.backend.entity.dto.MyRegionInfoResponseDto;
 import gitbal.backend.entity.dto.RegionListDto;
 import gitbal.backend.entity.dto.RegionListPageResponseDto;
 import gitbal.backend.service.RegionRankService;
@@ -10,6 +11,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,41 +23,38 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "지역 랭킹 API(구현중)", description = "지역 랭킹 페이지를 위한 api입니다.")
 public class RegionRankController {
 
-  private final RegionRankService regionRankService;
+    private final RegionRankService regionRankService;
 
-  @GetMapping("/myRegion")
-  @Operation(summary = "내 지역 정보 (미구현)", description = "내 지역 정보를 나타내는 api입니다.")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "내 지역 정보 요청을 성공했습니다."),
-      @ApiResponse(responseCode = "5xx", description = "내 지역 정보 요청을 실패했습니다.")
-  })
-  public void myRegion() { // TODO : 로그인한 유저로 봐야함.
+    @GetMapping("/myRegion")
+    @Operation(summary = "일부 구현 완료 -> 이후 fe 개발자 api 테스트 이후 케이스 따라 추가 수정 진행", description = "내 지역 정보를 나타내는 api입니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "내 지역 정보 요청을 성공했습니다."),
+        @ApiResponse(responseCode = "4xx", description = "내 지역 정보 요청을 실패했습니다.")
+    })
+    public ResponseEntity<MyRegionInfoResponseDto> myRegion(Authentication authentication) {
+        return ResponseEntity.ok(regionRankService.getMyRegionInfo(authentication));
+    }
 
-  }
-
-  @GetMapping("/firstregion")
-  @Operation(summary = "1등 지역 정보 (구현 완료)", description = "1등 지역 정보 탭 관련 api 요청입니다.")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "1등 지역 정보 요청을 성공했습니다."),
-      @ApiResponse(responseCode = "5xx", description = "1등 지역 정보 요청을 실패했습니다.")
-  })
-  public FirstRankRegionDto firstRankRegionInfo() {
-    return regionRankService.getFirstRankRegionnfo();
-  }
+    @GetMapping("/firstregion")
+    @Operation(summary = "1등 지역 정보 (구현 완료)", description = "1등 지역 정보 탭 관련 api 요청입니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "1등 지역 정보 요청을 성공했습니다."),
+        @ApiResponse(responseCode = "5xx", description = "1등 지역 정보 요청을 실패했습니다.")
+    })
+    public FirstRankRegionDto firstRankRegionInfo() {
+        return regionRankService.getFirstRankRegionnfo();
+    }
 
 
-  @GetMapping("/regionList")
-  @Operation(summary = "지역 리스트 (구현 완료)", description = "지역 리스트를 나타내는 api입니다.")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "지역 리스트 요청을 성공했습니다."),
-      @ApiResponse(responseCode = "5xx", description = "지역 리스트 요청을 실패했습니다.")
-  })
-  public RegionListPageResponseDto<RegionListDto> regionList() {
-    return regionRankService.getRegionList();
-  }
-
-//    /api/v1/regionRank/myRegion	get
-///api/v1/regionRank/regionList	get
+    @GetMapping("/regionList")
+    @Operation(summary = "지역 리스트 (구현 완료)", description = "지역 리스트를 나타내는 api입니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "지역 리스트 요청을 성공했습니다."),
+        @ApiResponse(responseCode = "5xx", description = "지역 리스트 요청을 실패했습니다.")
+    })
+    public RegionListPageResponseDto<RegionListDto> regionList() {
+        return regionRankService.getRegionList();
+    }
 
 
 }
