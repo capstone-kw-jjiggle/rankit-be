9a5e8811a8a7aaed6dc5b458c21646395a502e16
Khyojun <38220795+khyojun@users.noreply.github.com>
2024-05-16 21:31:15 +0900

    [hotfix] #78 - 잘못 반환되는 경우가 있어서 바로 수정
    RegionList 인데 SchoolListDto가 있었음.

--- src/main/java/gitbal/backend/entity/dto/RegionListDto.java
+++ src/main/java/gitbal/backend/entity/dto/RegionListDto.java
@@ -1,10 +1,11 @@
 package gitbal.backend.entity.dto;
 
+
 public record RegionListDto(String regionlName, Long regionScore, Long regionChangedScore,
                             String topContributorName) {
 
-  public static SchoolListDto of(String regionlName, Long regionScore, Long regionChangedScore,
+  public static RegionListDto of(String regionlName, Long regionScore, Long regionChangedScore,
       String topContributorName) {
-    return new SchoolListDto(regionlName, regionScore, regionChangedScore, topContributorName);
+    return new RegionListDto(regionlName, regionScore, regionChangedScore, topContributorName);
   }
 }
