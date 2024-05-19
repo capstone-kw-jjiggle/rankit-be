4f3f3b3aa1b1d82c8b5f2be454b7239508da69c2
Khyojun <khyojun1104@gmail.com>
2024-05-19 17:41:51 +0900

    [refactor] #86 - /api/v1/userInfo/{username} 비즈니스 로직 리팩토링 진행

--- src/main/java/gitbal/backend/service/UserInfoService.java
+++ src/main/java/gitbal/backend/service/UserInfoService.java
@@ -1,48 +1,27 @@
 package gitbal.backend.service;
 
-import gitbal.backend.entity.Region;
-import gitbal.backend.entity.School;
 import gitbal.backend.entity.User;
 import gitbal.backend.entity.dto.UserInfoDto;
-import gitbal.backend.exception.NotFoundRegionException;
-import gitbal.backend.exception.NotFoundSchoolException;
 import gitbal.backend.exception.NotFoundUserException;
-import gitbal.backend.repository.RegionRepository;
-import gitbal.backend.repository.SchoolRepository;
 import gitbal.backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.HibernateException;
-import org.springframework.dao.DataAccessException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserInfoService {
 
-  private final UserRepository userRepository;
-  private final RegionRepository regionRepository;
-  private final SchoolRepository schoolRepository;
+    private final UserRepository userRepository;
 
-  public ResponseEntity<UserInfoDto> getUserInfoByUserName(String userName) {
 
-    User user = userRepository.findByNickname(userName).orElseThrow(NotFoundUserException::new);
-
-    Long univId = user.getSchool().getId();
-    Long regionId = user.getRegion().getRegionId();
-    String imgName = user.getProfile_img().toString();
-    String userTitle = null;
-
-    Region userRegion = regionRepository.findById(regionId).orElseThrow(NotFoundRegionException::new);
-    School userUniv = schoolRepository.findById(univId).orElseThrow(NotFoundSchoolException::new);
-
-    String regionName = userRegion.getRegionName();
-    String univName = userUniv.getSchoolName();
-
-    UserInfoDto userInfoDto = new UserInfoDto(userName, univName, regionName, imgName, userTitle);
-
-    return ResponseEntity.ok(userInfoDto);
-
-  }
+    @Transactional(readOnly = true)
+    public ResponseEntity<UserInfoDto> getUserInfoByUserName(String userName) {
+        User user = userRepository.findByNickname(userName).
+            orElseThrow(NotFoundUserException::new);
+        UserInfoDto userInfoDto = UserInfoDto.of(user);
+        return ResponseEntity.ok(userInfoDto);
+    }
 
 }
