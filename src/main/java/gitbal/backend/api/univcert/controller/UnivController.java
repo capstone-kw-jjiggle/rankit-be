4fc261e2c8e8de55206701b72fecb67b1b8d6e42
leesj000603 <leesj000603@daum.net>
2024-06-05 13:05:43 +0900

    [refactor] #90 - univcert 반환값 dto로 변경 작업

--- src/main/java/gitbal/backend/api/univcert/controller/UnivController.java
+++ src/main/java/gitbal/backend/api/univcert/controller/UnivController.java
@@ -2,6 +2,7 @@ package gitbal.backend.api.univcert.controller;
 
 
 import gitbal.backend.api.univcert.dto.UnivCertCodeDto;
+import gitbal.backend.api.univcert.dto.UnivCertResponseDto;
 import gitbal.backend.api.univcert.dto.UnivCertStartDto;
 import gitbal.backend.api.univcert.service.UnivService;
 import gitbal.backend.global.exception.UnivCertCodeException;
@@ -34,8 +35,8 @@ public class UnivController {
       @ApiResponse(responseCode = "200", description = "대학 인증 요청을 성공했습니다."),
       @ApiResponse(responseCode = "400", description = "대학 인증 요청을 실패했습니다.")
   })
-  public ResponseEntity<Map<String, Object>> univRequestCertificate(@RequestBody UnivCertStartDto univCertStartDto) {
-    return ResponseEntity.ok(univSerivce.CertStart(univCertStartDto));
+  public ResponseEntity<UnivCertResponseDto> univRequestCertificate(@RequestBody UnivCertStartDto univCertStartDto) {
+    return ResponseEntity.ok(univSerivce.certStart(univCertStartDto));
   }
 
 
@@ -45,8 +46,8 @@ public class UnivController {
       @ApiResponse(responseCode = "200", description = "인증번호 검증을 성공했습니다."),
       @ApiResponse(responseCode = "400", description = "인증번호 검증을 실패했습니다.")
   })
-  public ResponseEntity<Map<String, Object>> univCertNumValidate(@RequestBody UnivCertCodeDto univCertCodeDto) {
-    return ResponseEntity.ok(univSerivce.CertCode(univCertCodeDto));
+  public ResponseEntity<UnivCertResponseDto> univCertNumValidate(@RequestBody UnivCertCodeDto univCertCodeDto) {
+    return ResponseEntity.ok(univSerivce.certCode(univCertCodeDto));
   }
 
 }
--- src/main/java/gitbal/backend/api/univcert/service/UnivService.java
+++ src/main/java/gitbal/backend/api/univcert/service/UnivService.java
@@ -2,13 +2,15 @@ package gitbal.backend.api.univcert.service;
 
 import com.univcert.api.UnivCert;
 import gitbal.backend.api.univcert.dto.UnivCertCodeDto;
+import gitbal.backend.api.univcert.dto.UnivCertResponseDto;
 import gitbal.backend.api.univcert.dto.UnivCertStartDto;
 import gitbal.backend.global.exception.UnivCertCodeException;
 import gitbal.backend.global.exception.UnivCertStartException;
-import java.io.IOException;
-import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class UnivService {
@@ -16,21 +18,34 @@ public class UnivService {
   @Value("${API-KEY}")
   private String apikey;
 
-  public Map<String, Object> CertStart(UnivCertStartDto univCertStartDto){
+  public UnivCertResponseDto certStart(UnivCertStartDto univCertStartDto){
     try {
-      return UnivCert.certify(apikey, univCertStartDto.getEmail(),univCertStartDto.getUnivName(), true);
+      Map<String, Object> response = UnivCert.certify(apikey, univCertStartDto.getEmail(), univCertStartDto.getUnivName(), true);
+      return responsetoDto(response, "인증번호 전송 완료");
     } catch (Exception e) {
-      throw new UnivCertStartException();
+      throw new UnivCertStartException(e.getMessage());
     }
-
   }
 
-  public Map<String, Object> CertCode(UnivCertCodeDto univCertCodeDto) {
+  public UnivCertResponseDto certCode(UnivCertCodeDto univCertCodeDto) {
     try {
-      return UnivCert.certifyCode(apikey, univCertCodeDto.getEmail(), univCertCodeDto.getUnivName(), univCertCodeDto.getCode());
+      Map<String, Object> response = UnivCert.certifyCode(apikey, univCertCodeDto.getEmail(), univCertCodeDto.getUnivName(), univCertCodeDto.getCode());
+      return responsetoDto(response, "인증 성공");
+
     } catch (Exception e) {
       throw new UnivCertCodeException();
     }
   }
-}
 
+  private UnivCertResponseDto responsetoDto(Map<String, Object> univCertResponse, String successMessage) {
+    boolean success = Boolean.TRUE.equals(univCertResponse.get("success"));
+    int status = success ? 200 : (Integer) univCertResponse.get("code");
+    String message = success ? successMessage : (String) univCertResponse.get("message");
+
+    return UnivCertResponseDto.builder()
+        .status(status)
+        .success(success)
+        .message(message)
+        .build();
+  }
+}
