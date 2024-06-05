36f1c8a30b72366d985e64b2a8705643e11361df
leesj000603 <leesj000603@daum.net>
2024-06-05 15:40:17 +0900

    [feat] #120 - SchedulingService에 랭킹 업데이트 되도록 적용

--- src/main/java/gitbal/backend/schedule/SchedulingService.java
+++ src/main/java/gitbal/backend/schedule/SchedulingService.java
@@ -3,6 +3,7 @@ package gitbal.backend.schedule;
 import gitbal.backend.schedule.userupdate.majorLanguage.UserLanguagesUpdater;
 import gitbal.backend.schedule.userupdate.onedaycommit.UserOneDayCommitUpdater;
 import gitbal.backend.schedule.userupdate.score.UserScoreUpdater;
+import gitbal.backend.schedule.userupdate.userRank.RankUpdater;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +18,7 @@ public class SchedulingService {
     private final UserOneDayCommitUpdater userOneDayCommitUpdater;
     private final UserScoreUpdater userScoreUpdater;
     private final UserLanguagesUpdater userLanguagesUpdater;
+    private final RankUpdater rankUpdater;
 
 
     @Scheduled(initialDelay = 2, fixedRate = 360, timeUnit = TimeUnit.MINUTES)
@@ -31,5 +33,6 @@ public class SchedulingService {
     @Scheduled(initialDelay = 2, fixedRate = 360, timeUnit = TimeUnit.MINUTES)
     public void updateUserLanguages(){userLanguagesUpdater.update();}
 
-
+    @Scheduled(initialDelay = 3, fixedRate = 360, timeUnit = TimeUnit.MINUTES)
+    public void updateRanks(){rankUpdater.update();}
 }
