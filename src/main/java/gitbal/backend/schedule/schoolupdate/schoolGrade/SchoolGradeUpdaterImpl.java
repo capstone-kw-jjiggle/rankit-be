c87ab8e4e34936cbbf1e6cf879444f900b0aa181
leesj000603 <leesj000603@daum.net>
2024-06-13 10:42:32 +0900

    [feat] #127 - 스케줄러의 업데이트 함수 실행될 때 와 완료되었을 때  로그 추가

--- src/main/java/gitbal/backend/schedule/schoolupdate/schoolGrade/SchoolGradeUpdaterImpl.java
+++ src/main/java/gitbal/backend/schedule/schoolupdate/schoolGrade/SchoolGradeUpdaterImpl.java
@@ -1,6 +1,7 @@
 package gitbal.backend.schedule.schoolupdate.schoolGrade;
 
 import gitbal.backend.domain.school.SchoolService;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -12,7 +13,10 @@ public class SchoolGradeUpdaterImpl implements SchoolGradeUpdater {
   private final SchoolService schoolService;
 
   @Override
+  @Transactional
   public void update() {
+    log.info("[schedulingSchoolGrade] method start");
     schoolService.updateSchoolGrade();
+    log.info("[schedulingSchoolGrade] method finish");
   }
 }
