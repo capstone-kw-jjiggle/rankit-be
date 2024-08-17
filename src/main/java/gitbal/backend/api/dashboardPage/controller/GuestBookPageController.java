a3e60c94d7fd7ac7de3d69e2b84776129dfd7fcf
Khyojun <khyojun1104@gmail.com>
2024-08-17 19:19:06 +0900

    [fix] #168 - swaggerAPI 설명 수정

--- src/main/java/gitbal/backend/api/dashboardPage/controller/GuestBookPageController.java
+++ src/main/java/gitbal/backend/api/dashboardPage/controller/GuestBookPageController.java
@@ -28,8 +28,8 @@ public class GuestBookPageController {
     @GetMapping
     @Operation(summary = "현재 등록되어져있는 방명록들 최신 순으로 30개 표시 ", description = "방명록 목록을 보여주는 api입니다.")
     @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "게시판 목록 전달 성공"),
-        @ApiResponse(responseCode = "500", description = "게시판 목록을 서버 측에서 오류가 발생하여 전달하지 못했습니다.")
+        @ApiResponse(responseCode = "200", description = "방명록 목록 전달 성공"),
+        @ApiResponse(responseCode = "500", description = "방명록 목록을 서버 측에서 오류가 발생하여 전달하지 못했습니다.")
     })
     public ResponseEntity<List<GuestBookResponseDto>> getGuestBooks() {
         return ResponseEntity.ok(guestBookPageFacade.getDashBoardDtos());
