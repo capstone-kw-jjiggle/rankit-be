338f52a2f6e47757077236b90454664bf4544a61
Khyojun <khyojun1104@gmail.com>
2024-08-19 23:59:58 +0900

    [fix] #174 - OneDayCommit 관련 코드 삭제작업 진행!

--- src/main/java/gitbal/backend/domain/onedaycommit/infra/OneDayCommitJpaRepository.java
+++
@@ -1,8 +0,0 @@
-package gitbal.backend.domain.onedaycommit.infra;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OneDayCommitJpaRepository extends JpaRepository<OneDayCommitJpaEntity, Long> {
-
-}
