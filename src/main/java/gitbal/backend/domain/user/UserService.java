82a4a0f02040615ae4c9bc5f23f026ec3e905240
leesj <leesj000603@daum.net>
2024-08-21 11:32:49 +0900

    [refator] #171 - User 도메인으로 UserCalculator 메서드 이동 후 UserCalculator 삭제

--- src/main/java/gitbal/backend/domain/user/UserService.java
+++ src/main/java/gitbal/backend/domain/user/UserService.java
@@ -210,5 +210,34 @@ public class UserService {
         }
     }
 
+    public List<User> getAllUserExceptCurrentUser(User user) {
+        List<User> users = userRepository.findAll();
+        users.remove(user);
+        return users;
+    }
+
+    public Grade getNextGrade(User user) {
+        Grade grade = user.getGrade();
+
+        switch (grade) {
+            case YELLOW -> {
+                return Grade.GREEN;
+            }
+            case GREEN -> {
+                return Grade.BLUE;
+            }
+            case BLUE -> {
+                return Grade.RED;
+            }
+            case RED -> {
+                return Grade.GREY;
+            }
+            case GREY, PURPLE -> {
+                return Grade.PURPLE;
+            }
+            default -> throw new IllegalArgumentException("알 수 없는 등급입니다: " + grade);
+        }
+    }
+
 
 }
