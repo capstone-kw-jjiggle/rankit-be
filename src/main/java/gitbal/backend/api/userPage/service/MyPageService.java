1c132bea985d75c7aeda4c1c7830345b41dbf189
leesj <leesj000603@daum.net>
2024-08-21 11:34:18 +0900

    [refactor] #171 - f리팩토링 상황에 맞게 MyPageService의 친구추천 메서드 수정

--- src/main/java/gitbal/backend/api/userPage/service/MyPageService.java
+++ src/main/java/gitbal/backend/api/userPage/service/MyPageService.java
@@ -1,14 +1,11 @@
 package gitbal.backend.api.userPage.service;
 
-import gitbal.backend.api.userPage.RandomUserPicker;
-import gitbal.backend.api.userPage.UserCalculator;
+import gitbal.backend.api.userPage.RandomFriendPicker;
 import gitbal.backend.api.userPage.dto.FriendSuggestDTO;
 import gitbal.backend.domain.region.application.RegionService;
 import gitbal.backend.domain.school.SchoolService;
 import gitbal.backend.domain.user.User;
-import gitbal.backend.domain.user.UserRepository;
 import gitbal.backend.domain.user.UserService;
-import gitbal.backend.global.constant.Grade;
 import gitbal.backend.global.exception.NotLoginedException;
 import gitbal.backend.global.security.CustomUserDetails;
 import java.util.ArrayList;
@@ -27,8 +24,7 @@ public class MyPageService {
   private final UserService userService;
   private final SchoolService schoolService;
   private final RegionService regionService;
-  private final RandomUserPicker randomUserPicker;
-  private final UserCalculator userCalculator;
+  private final RandomFriendPicker randomUserPicker;
 
   //지역이나 학교 수정해도 수정하기 전 후의 지역점수나 학교점수에 유저 점수 직접 더하고 빼지 않음. (어짜피 점수 업데이트 할 때 반영되니깐)
   @Transactional
@@ -55,32 +51,7 @@ public class MyPageService {
   }
 
   private List<User> getResultFriends(User user){
-    Grade grade = userCalculator.getNextGrade(user.getGrade());
-    String lang = user.getMajorLanguage().getMajorLanguage();
-    Long score = user.getScore();
-
-    List<User> allUsers = userCalculator.getAllUserExceptCurrentUser(user);
-
-    User purpleUser = randomUserPicker.getRandomHigestGradeUser(allUsers);
-    User sameLangUser = randomUserPicker.getRandomSameLanguageUser(allUsers, lang);
-    User nextGradeUser = randomUserPicker.getRandomNextLanguageUser(allUsers, grade);
-    User nearScoreUser = randomUserPicker.getRandomNearScoreUser(allUsers, score);
-
-    List<User> result = new ArrayList<>();
-    if (purpleUser != null) {
-      result.add(purpleUser);
-    }
-    if (sameLangUser != null) {
-      result.add(sameLangUser);
-    }
-    if (nextGradeUser != null) {
-      result.add(nextGradeUser);
-    }
-    if (nearScoreUser != null) {
-      result.add(nearScoreUser);
-    }
-
-    return result;
+    return randomUserPicker.getFriendList(user);
   }
 
   private FriendSuggestDTO convertToDTO(User user) {
