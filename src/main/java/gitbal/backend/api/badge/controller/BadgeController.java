38bf911560a2e45c1321de0c1cf5d9fca41601ff
leesj000603 <leesj000603@daum.net>
2024-09-29 18:31:44 +0900

    [feat] #182 - 성공한 경우에도 상태코드 넘기도록

--- src/main/java/gitbal/backend/api/badge/controller/BadgeController.java
+++ src/main/java/gitbal/backend/api/badge/controller/BadgeController.java
@@ -8,9 +8,11 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1/badge")
@@ -26,7 +28,7 @@ public class BadgeController {
       @ApiResponse(responseCode = "200", description = "유저 정보를 가져오는데 성공했습니다."),
       @ApiResponse(responseCode = "400", description = "유저 정보를 가져오는데 실패했습니다.")
   })
-  public BadgeResponseDTO getBadge(@RequestParam String username) {
-    return badgeService.getBadgeResponse(username);
+  public ResponseEntity<BadgeResponseDTO> getBadge(@RequestParam String username) {
+    return ResponseEntity.ok(badgeService.getBadgeResponse(username));
   }
 }
