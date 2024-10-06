c7d139df1bab2da4186583bb3e48749652a072eb
Khyojun <khyojun1104@gmail.com>
2024-10-06 23:33:07 +0900

    [fix] #199 - mainPageController 관련 swagger 설명 수정 진행

--- src/main/java/gitbal/backend/api/mainPage/controller/MainPageController.java
+++ src/main/java/gitbal/backend/api/mainPage/controller/MainPageController.java
@@ -27,17 +27,19 @@ public class MainPageController {
     @Operation(summary = "메인 페이지 유저들에 대한 API입니다.", description = "메인에 나타나는 유저들 페이지당 14명씩")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "유저들을 가져오는데 성공했습니다."),
-        @ApiResponse(responseCode = "400", description = "유저들을 가져오는데 실패했습니다.")
+        @ApiResponse(responseCode = "400", description = "유저들을 가져오는데 실패했습니다."),
+        @ApiResponse(responseCode = "400", description = "잘못된 페이지를 요청한 것입니다.")
     })
     public ResponseEntity<MainPageUserResponseDto> users(@RequestParam(required = false, defaultValue = "1") int page,  @RequestParam(required = false) String searchedname) {
         return ResponseEntity.ok(mainPageService.getUsers(page, searchedname));
     }
 
     @GetMapping("/{grade}/users")
-    @Operation(summary = "메인 페이지 등급별 유저들에 대한 API입니다.", description = "메인에 나오는 등급별 유저 페이지당 14명씩")
+    @Operation(summary = "메인 페이지 등급별 유저들에 대한 API입니다.(새로 업데이트된 API)", description = "메인에 나오는 등급별 유저 페이지당 14명씩")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "유저들을 가져오는데 성공했습니다."),
-        @ApiResponse(responseCode = "400", description = "유저들을 가져오는데 실패했습니다.")
+        @ApiResponse(responseCode = "400", description = "유저들을 가져오는데 실패했습니다."),
+        @ApiResponse(responseCode = "400", description = "잘못된 페이지를 요청한 것입니다.")
     })
     public ResponseEntity<MainPageUserResponseDto> users(@RequestParam(required = false, defaultValue = "1") int page,  @PathVariable Grade grade) {
         return ResponseEntity.ok(mainPageService.getGradeUsers(page, grade));
