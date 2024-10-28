2eef9dfff76a4b1fe9c1c5ed7e1bee3a611c028a
Khyojun <khyojun1104@gmail.com>
2024-10-28 23:36:20 +0900

    [feat] #217 - 비동기 로직 변경작업 진행 -> 동시에 비동기로 진행할 때의 점수 업데이트 반영 안되는 문제
    - 영속성 컨텍스트가 스레드 당 1개씩 되는데 점수가 변경된 커밋이 나중에 진행되므로 스레드 a, b 에 적용될때 b 에서는 점수가 변경되지 않은 상태로 커밋이 되어지기 때문에 시점을 맞추지 못하면은 점수가 업데이트 되지 않는 문제가 발생해서 하나로 합치게 되었다.

--- src/main/java/gitbal/backend/schedule/SchedulingService.java
+++ src/main/java/gitbal/backend/schedule/SchedulingService.java
@@ -5,14 +5,13 @@ import gitbal.backend.schedule.userupdate.majorLanguage.UserLanguagesUpdater;
 import gitbal.backend.schedule.userupdate.score.UserScoreUpdater;
 import gitbal.backend.schedule.userupdate.userGrade.UserGradeUpdater;
 import gitbal.backend.schedule.userupdate.userRank.UserRankUpdater;
-import java.time.LocalDateTime;
 import java.util.concurrent.TimeUnit;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,30 +24,15 @@ public class SchedulingService {
     private final UserRankUpdater userRankUpdater;
     private final UserGradeUpdater userGradeUpdater;
 
-
-    @Async("taskAExecutor")
     @Scheduled(initialDelay = 1, fixedRate = 360, timeUnit = TimeUnit.MINUTES) // fixedRate를 사용하여 일정한 6시간의 주기를 가지는것이 중요!
-    @SchedulerLock(name="updateUserScore")
-    public void updateUserScore() {
+    @SchedulerLock(name="updateUser")
+    @Transactional
+    public void updateUser() {
         userScoreUpdater.update();
         userRankUpdater.update();
         schoolRankUpdater.update();
         userGradeUpdater.update();
-        log.info("taskA- {} - {}", LocalDateTime.now(), Thread.currentThread().getName());
-    }
-
-    @Async("taskBExecutor")
-    @Scheduled(initialDelay = 1, fixedRate = 360, timeUnit = TimeUnit.MINUTES) // fixedRate를 사용하여 일정한 6시간의 주기를 가지는것이 중요!
-    @SchedulerLock(name = "updateUserLanguages")
-    public void updateUserLanguages() {
         userLanguagesUpdater.update();
-        log.info("taskBUpdateUserLanguages - {} - {}", LocalDateTime.now(), Thread.currentThread().getName());
     }
-
-
-
-
-
-
 
 }
