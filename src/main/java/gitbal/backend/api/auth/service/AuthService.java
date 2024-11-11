93a2aaa425b9c492c96f10d35613d5147d26d8ae
Khyojun <khyojun1104@gmail.com>
2024-11-11 23:13:37 +0900

    [test] isRegistered join 하면 적용될 수 있게 변경

--- src/main/java/gitbal/backend/api/auth/service/AuthService.java
+++ src/main/java/gitbal/backend/api/auth/service/AuthService.java
@@ -47,9 +47,9 @@ public class AuthService {
 
         User findUser = userRepository.findByNickname(nickname)
             .orElseThrow(() -> new JoinException("유저가 존재하지 않습니다."));
-//        if(Boolean.FALSE.equals(findUser.getFirstLogined()))
-//            findUser.toggleLogined();
-//      //  GitbalApiDto gitbalApiDto = GitbalApiDto.of(userService.calculateUserScore(nickname));
+        if(Boolean.FALSE.equals(findUser.getFirstLogined()))
+            findUser.toggleLogined();
+      //  GitbalApiDto gitbalApiDto = GitbalApiDto.of(userService.calculateUserScore(nickname));
 
         //loginRequestDto 학교이름, 지역이름, 프로필 이미지 이름
         UserDto userDto = initUserDto(joinRequestDto, nickname);
