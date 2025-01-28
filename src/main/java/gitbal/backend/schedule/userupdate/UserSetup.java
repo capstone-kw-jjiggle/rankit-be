139460630cb43802a097a59e0a91e2bb71c54d0b
Khyojun <khyojun1104@gmail.com>
2025-01-29 01:49:51 +0900

    [fix] #249 - 유저 확보로 인한 MockDataGenerator 관련 로직 수정 및 삭제

--- src/main/java/gitbal/backend/schedule/userupdate/UserSetup.java
+++ src/main/java/gitbal/backend/schedule/userupdate/UserSetup.java
@@ -1,7 +1,6 @@
 package gitbal.backend.schedule.userupdate;
 
 import gitbal.backend.domain.user.UserService;
-import gitbal.backend.global.mock.GenerateRealMockUser;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -9,14 +8,11 @@ import java.util.stream.Collectors;
 
 public abstract class UserSetup {
 
-    private final GenerateRealMockUser generateRealMockUser = new GenerateRealMockUser();
 
     protected List<String> getAllUsernames(UserService userService){
-        List<String> allUserNamesExcludeMockData = userService.findAllUserNames();
+        List<String> userNames = userService.findAllUserNames();
 
-        return allUserNamesExcludeMockData.stream()
-                .filter(u -> !generateRealMockUser.getNames().contains(u))
-                .collect(Collectors.toList());
+        return userNames.stream().toList();
     }
 
 }
